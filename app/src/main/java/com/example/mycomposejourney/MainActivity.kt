5d366649df810cb4b07901ae1bfa955f639658d5
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-01-15 18:25:19 +0100

    Feat: Composable card with background gradient

--- app/src/main/java/com/example/mycomposejourney/MainActivity.kt
+++ app/src/main/java/com/example/mycomposejourney/MainActivity.kt
@@ -3,43 +3,89 @@ package com.example.mycomposejourney
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier
-                .background(Color(0xFFF2F2F2))
-                .fillMaxWidth()
-                .fillMaxHeight(.5f)
-                .border(5.dp, Color.Magenta)
-                .padding(20.dp)
-                .border(1.dp, Color.Red)
-                .padding(20.dp)
-                .border(10.dp, Color.Blue)
-                .padding(20.dp)
-                .border(2.dp, Color.Yellow)
-                .padding(30.dp)
+            val painter = painterResource(id = R.drawable.pasta)
+            val description = "Italian pasta"
+            val title = "Respondo Pasta"
+            val price = "$5"
 
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
             ) {
-                Text("Bye Bye XML", modifier = Modifier
-                    .offset(150.dp, 19.dp))
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("Hey, Compose 🥰")
-                Button(onClick = {  }, modifier = Modifier.offset(0.dp, 20.dp)) {
-                    Text("Sweet Alert")
-                }
+                ImageCard(painter, title, price, description)
             }
+        }
+    }
+}
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    title: String,
+    price: String,
+    contentDes: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDes,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 350f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
 
+                Row(
+                    modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text(title, style = TextStyle(color = Color.White, fontSize = 18.sp))
+                    Text(price, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+
+            }
         }
     }
 }
