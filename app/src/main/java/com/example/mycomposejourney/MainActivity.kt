a3feb8e8fe0ba9c69425977b882c1a62d590b3b9
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-01-22 01:41:59 +0100

    Feat: change default background color on clikc

--- app/src/main/java/com/example/mycomposejourney/MainActivity.kt
+++ app/src/main/java/com/example/mycomposejourney/MainActivity.kt
@@ -3,36 +3,46 @@ package com.example.mycomposejourney
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mycomposejourney.ui.theme.MyComposeJourneyTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeJourneyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+
+            ColorChangerBox(Modifier.fillMaxSize())
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MyComposeJourneyTheme {
-        Greeting("Android")
+fun ColorChangerBox(modifier: Modifier = Modifier) {
+    val color = remember {
+        mutableStateOf(Color.Yellow)
+    }
+
+    Box(modifier
+        .background(color.value)
+        .clickable {
+                color.value = Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            }) {
+
     }
 }
+
+
