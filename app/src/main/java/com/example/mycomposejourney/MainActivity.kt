c5c4b6b81eb355bab0f08a59387d9fa2d8a2ca5f
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-01-26 21:08:15 +0100

    Feat: state, buttons, textfileds, snackbars

--- app/src/main/java/com/example/mycomposejourney/MainActivity.kt
+++ app/src/main/java/com/example/mycomposejourney/MainActivity.kt
@@ -3,68 +3,57 @@ package com.example.mycomposejourney
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import kotlin.random.Random
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.fillMaxSize()) {
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-                ColorChangerBox(
-                    Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                ) {
-                    color.value = it
-                }
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember { mutableStateOf("") }
+            val scope = rememberCoroutineScope()
 
-                Box(
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
                     modifier = Modifier
-                        .background(color.value)
                         .fillMaxSize()
-                        .weight(1f)
+                        .padding(horizontal = 30.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text("Enter any name")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
 
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("$textFieldState is your girlfriend right? 😂", "Yes")
+                        }
+                    }) {
+                        Text("Click me to see a magic")
+                    }
                 }
             }
         }
     }
 }
-
-
-@Composable
-fun ColorChangerBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit) {
-
-    Box(
-        modifier
-            .background(Color.Blue)
-            .clickable {
-                updateColor(
-                    Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        1f
-                    )
-                )
-            }) {
-
-    }
-}
-
-
